83f2ee0f949919fec23adc7612d77dd6fd40c3c2
Ian Kilgore <iank@iank.org>
2025-12-30 15:52:25 -0500

    Problem statements for exercises 4 and 5 (TODO)

--- Topology/Ch1_4.lean
+++ Topology/Ch1_4.lean
@@ -1,4 +1,4 @@
-import Mathlib
+import Mathlib.Data.Real.Basic
 
 open Set
 
@@ -237,7 +237,49 @@ theorem ex3b : ⋃ α ∈ J, A α ⊆ ⋃ α, A α := by
   exact hxAj.right
 end
 
--- Exercise 4 (TODO)
--- Exercise 5 (TODO)
+section
+-- Exercise 4.
+-- Let A_α (α ∈ I) be an indexed family of subsets of a set S. Let B ⊆ S.
+variable {S I : Type}
+variable {A : I → Set S}
+variable (B : Set S)
+-- Prove that:
+-- a) B ⊆ ⋂ A α IFF ∀ β ∈ I, B ⊆ A β
+theorem ex4a : B ⊆ ⋂ α, A α ↔ ∀ (β : I), B ⊆ A β := by
+  sorry
+
+-- b) ⋃ A α ⊆ B IFF ∀ β ∈ I, A β ⊆ B
+theorem ex4b : ⋃ α, A α ⊆ B ↔ ∀ (β : I), A β ⊆ B := by
+  sorry
+end
+
+section
+-- Exercise 5.
+-- Let I be the set of real numbers that are greater than 0.
+-- For each x ∈ I, let A_x be the open interval (0, x).
+-- For each x ∈ I, let B_x be the closed interval [0, x].
+abbrev I := {x : ℝ // 0 < x}
+def A (x : I) : Set ℝ := {y : ℝ | 0 < y ∧ y < (x : ℝ)}
+def B (x : I) : Set ℝ := {y : ℝ | 0 ≤ y ∧ y ≤ (x : ℝ)}
+
+-- Prove that:
+-- i)   ⋂ A_x = ∅
+
+theorem ex5_i : (⋂ x : I, A x) = (∅ : Set ℝ) := by
+  sorry
+
+-- ii)  ⋃ A_x = I
+theorem ex5_ii : (⋃ x : I, A x) = {y : ℝ | 0 < y} := by
+  sorry
+
+-- iii) ⋂ B_x = {0}
+theorem ex5_iii : (⋂ x : I, B x) = ({0} : Set ℝ) := by
+  sorry
+
+-- iv)  ⋃ B_x = I ∪ {0}
+theorem ex5_iv : (⋃ x : I, B x) = ({y : ℝ | 0 < y} ∪ {0}) := by
+  sorry
+
+end
 
 end Ch1_4

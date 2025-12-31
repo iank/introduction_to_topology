df61c2deaa2478ea6ea606a0c79b17b25e20336b
Ian Kilgore <iank@iank.org>
2025-12-31 17:44:23 -0500

    Exercises 2 and 3

--- Topology/Ch1_5.lean
+++ Topology/Ch1_5.lean
@@ -1,4 +1,5 @@
 import Mathlib
+import Mathlib.Data.Set.Card
 open Set
 
 namespace Ch1_5
@@ -52,7 +53,78 @@ theorem ex1' :
       simp_all
 end
 
--- Exercise 2 (TODO)
--- Exercise 3 (TODO)
+-- Exercise 2: Prove that if A has precisely n distinct elements and B has
+-- precisely m distinct elements, where m and n are positive integers, then A ×
+-- B has precisely mn distinct elements.
+
+-- I realize this basically just invokes a library proof of the thing we're trying to
+-- prove but I'm not sure how to do it more fundamentally...
+theorem ex2
+  {α β : Type _} (A : Set α) (B : Set β) {n m : ℕ} (hA : A.ncard = n) (hB : B.ncard = m) :
+  (A ×ˢ B).ncard = n * m := by
+    rw [← hA]
+    rw [← hB]
+    exact Set.ncard_prod
+
+-- Exercise 3. Let A and B be sets, both of which have at least two distinct
+-- members. Prove that there is a subset W ⊆ A × B that is not the Cartesian
+-- product of a subset of A with a subset of B. [Thus, not every subset of a
+-- Cartesian product is the Cartesian product of a pair of subsets.]
+set_option linter.style.commandStart false
+theorem ex3
+    {α β : Type*} (A : Set α) (B : Set β)       -- Let A and B be sets
+    (hA : ∃ a₁ ∈ A, ∃ a₂ ∈ A, a₁ ≠ a₂)          -- Two distinct members in A
+    (hB : ∃ b₁ ∈ B, ∃ b₂ ∈ B, b₁ ≠ b₂) :        -- Two distinct members in B
+    ∃ W : Set (α × β), W ⊆ A ×ˢ B               -- There is a subset W ⊆ A × B
+        ∧ ¬ ∃ A' : Set α, ∃ B' : Set β, W = A' ×ˢ B' := by    -- Where W is not A' × B'
+    -- We can prove this by constructing an example:
+    -- {(a1, b1), (a1, b2), (a2, b1), (a2, b2)} ⊆ A × B
+    -- W = {(a1, b2), (a2, b1)}
+    rcases hA with ⟨a1, ha1A, a2, ha2A, ha12⟩
+    rcases hB with ⟨b1, hb1B, b2, hb2B, hb12⟩
+    let W : Set (α × β) := {p | (p.1 = a1 ∧ p.2 = b2) ∨ (p.1 = a2 ∧ p.2 = b1)}
+    use W
+    constructor
+    · -- Prove W ⊆ A × B
+      intro x hx
+      simp only [mem_setOf_eq, W] at hx -- hx is now an explicit list of what (x.1, x.2) could be
+      simp only [mem_prod]              -- We must show x.1 ∈ A and x.2 ∈ B
+      rcases hx with hxW1 | hxW2
+      · rcases hxW1 with ⟨hx1a1, hx2b2⟩
+        subst hx1a1
+        subst hx2b2
+        exact ⟨ha1A, hb2B⟩
+      · rcases hxW2 with ⟨hx1a2, hx2b1⟩
+        subst hx1a2
+        subst hx2b1
+        exact ⟨ha2A, hb1B⟩
+    · -- Prove W is not a subset of A' × B' (subsets of A and B)
+      simp only [not_exists]
+      intro A' B' hW          -- Assume there is a product W = A' × B'
+      -- Since (a1,b2) ∈ W and (a2,b1) ∈ W
+      -- we'd have a1 ∈ A'
+      have ha1A' : a1 ∈ A' := by                    -- Just unpacking / matching here
+        have ha1b2W : (a1, b2) ∈ W := by
+          simp [W]
+        have ha1b2A'B' : (a1, b2) ∈ A' ×ˢ B' := by
+          simpa [hW] using ha1b2W
+        rcases ha1b2A'B' with ⟨ha1A', hb2B'⟩
+        exact ha1A'
+      -- And b1 ∈ B'
+      have hb1B' : b1 ∈ B' := by                    -- Just unpacking / matching here
+        have ha2b1W : (a2, b1) ∈ W := by
+          simp [W]
+        have ha2b1A'B' : (a2, b1) ∈ A' ×ˢ B' := by
+          simpa [hW] using ha2b1W
+        rcases ha2b1A'B' with ⟨ha2A', hb1B'⟩
+        exact hb1B'
+      -- That would mean (a1, b1) is in A' × B'
+      have ha1b1A'B' : ⟨a1, b1⟩ ∈ A' ×ˢ B' := by
+        simp only [mem_prod]
+        exact ⟨ha1A', hb1B'⟩
+      -- ie, it is in W
+      rw [← hW] at ha1b1A'B'
+      -- But it is not
+      simp [W, ha12, hb12] at ha1b1A'B'
 
 end Ch1_5

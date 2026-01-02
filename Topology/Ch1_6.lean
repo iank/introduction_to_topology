05602e91e46387dd8c2237a432e6e6e54957247a
Ian Kilgore <iank@iank.org>
2026-01-02 03:36:32 -0500

    Exercise 1c, 1d

--- Topology/Ch1_6.lean
+++ Topology/Ch1_6.lean
@@ -38,8 +38,51 @@ theorem ex1b (f : A → B) (Y : Set B) : Y ⊇ f '' (f ⁻¹' Y) := by
   rewrite [hfxy] at hfxY
   exact hfxY
 
--- c) TODO
--- d) TODO
+-- c) If f : A → B is one-one, then for each subset X ∈ A, f⁻¹(f(X)) = X
+theorem ex1c (f : A → B) (X : Set A) (hf : Function.Injective f) : f ⁻¹' (f '' X) = X := by
+  -- We could use simp [hf, preimage_image_eq] but that's cheating at this point...
+  ext x
+  constructor
+  · -- Show x ∈ f⁻¹(f(X)) → x ∈ X
+    suffices f x ∈ f '' X → x ∈ X by simpa [mem_preimage]
+    suffices (∃ x' ∈ X, f x' = f x) → x ∈ X by simpa [mem_image]
+    -- Since f is injective, if f x' = f x then x' = x
+    intro hx
+    rcases hx with ⟨x', hx'X, hfx'x⟩
+    have : x' = x := hf hfx'x
+    -- And x' ∈ X so x ∈ X
+    rw [← this]
+    exact hx'X
+  · -- Show x ∈ X → x ∈ f⁻¹(f(X))
+    -- This is simpler.
+    intro hx
+    use x
+
+-- d) If f : A → B is onto, then for each subset Y ⊆ B, f(f⁻¹(Y)) = Y
+theorem ex1d (f : A → B) (Y : Set B) (hf : Function.Surjective f) : f '' (f ⁻¹' Y) = Y := by
+  -- Again, we could use simp [hf, image_preimage_eq]..
+  ext y
+  constructor
+  · -- Show y ∈ f(f⁻¹(Y)) → y ∈ Y
+    intro hy
+    -- y ∈ f(f⁻¹(Y)) means ∃ x ∈ X s.t. f(x) ∈ Y and f(x) = y
+    simp only [mem_image, mem_preimage] at hy
+    rcases hy with ⟨x, hfxY, hfxy⟩
+    -- f(x) = y and f(x) ∈ Y,
+    rw [hfxy] at hfxY
+    -- so y ∈ Y
+    exact hfxY
+  · -- Show y ∈ Y → y ∈ f(f⁻¹(Y))
+    intro hy
+    -- ie, ∃ x ∈ f ⁻¹' Y, f x = y
+    -- ie, ∃ x, f x ∈ Y ∧ f x = y
+    simp only [mem_image, mem_preimage]
+    -- f is surjective so ∃ a, f a = y
+    rcases hf y with ⟨a, hfay⟩
+    -- let x = a, so f(a) = y
+    use a
+    rw [hfay]
+    exact ⟨hy, rfl⟩
 end
 
 -- Exercise 2: TODO

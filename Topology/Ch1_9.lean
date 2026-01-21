408eff120c0002f7df92c8381704a45199aed7f0
Ian Kilgore <iank@iank.org>
2026-01-21 07:28:46 -0500

    Exercise 4

--- Topology/Ch1_9.lean
+++ Topology/Ch1_9.lean
@@ -262,7 +262,73 @@ theorem extension_trans' {A B : Set X} (hAB : A ⊆ B)
 end ex3
 
 namespace ex4
--- TODO: exercise 4
+-- Exercise 4
+variable {X Y : Type*} [Fintype X] [Fintype Y] [DecidableEq X] [DecidableEq Y]
+
+-- Let m, n be positive integers. Let X be a set with m distinct elements and Y
+-- a set with n distinct elements.
+
+-- a) How many distinct functions are there from X to Y?
+theorem num_functions (hX : Fintype.card X = m) (hY : Fintype.card Y = n) :
+    Fintype.card (X → Y) = n ^ m := by
+  rw [← hX]
+  rw [← hY]
+  -- Proved something like this explicitly in Ch1.6, ex3 so I'm comfortable just
+  -- using Fintype.card_fun here.
+  exact Fintype.card_fun (α := X) (β := Y)
+
+-- b) Let A be a subset of X with r distinct elements, 0 ≤ r < m, and
+-- f: A → Y. How many distinct extensions of f to X are there?
+theorem num_extensions {A : Set X} [DecidablePred (· ∈ A)] [Fintype A]
+    (f : A → Y)
+--  (hr : r < m)
+    (hA : Fintype.card A = r)
+    (hX : Fintype.card X = m)
+    (hY : Fintype.card Y = n) :
+    Fintype.card { F : X → Y // ∀ a : A, F a = f a } = n ^ (m - r) := by
+  -- f is given, so I think the number of distinct extensions should be defined
+  -- by the number of distinct inclusion mappings. The inclusion mappings can
+  -- differ only in X \ A, the cardinality of which is (m - r)
+  have hXcompl := Fintype.card_compl_set A
+  rw [hA] at hXcompl
+  rw [hX] at hXcompl
+  -- So the cardinality of the Aᶜ → Y is n ^ (m - r)
+  have hXcomplY := Fintype.card_fun (α := ↑Aᶜ) (β := Y)
+  rw [hXcompl] at hXcomplY
+  rw [hY] at hXcomplY
+  -- Establish the equivalence between the set of extensions F and Aᶜ → Y
+  let e : { F : X → Y // ∀ a : A, F a = f a } ≃ (↑Aᶜ → Y) := {
+    toFun := fun ⟨F, hF⟩ => fun x => F x
+    invFun := fun g => ⟨fun x => if h : x ∈ A then f ⟨x, h⟩ else g ⟨x, h⟩, ?invFun_proof⟩
+    left_inv := ?left_inv_proof
+    right_inv := ?right_inv_proof
+  }
+  case invFun_proof =>
+    -- Just need to show this satisfies the extension predicate, ie F a = f a
+    -- Since the piecewise function we defined is f(a) when a ∈ A, this is trivial
+    intro a
+    simp [a.property]
+  case left_inv_proof =>
+    -- Show that invFun(toFun(x)) = x
+    intro ⟨F, hF⟩
+    simp only [Subtype.mk.injEq]
+    funext x
+    split_ifs with h
+    case pos => exact (hF ⟨x, h⟩).symm
+    case neg => rfl
+  case right_inv_proof =>
+    -- Show that toFun(invFun(x)) = x
+    intro g
+    funext x
+    simp only [Subtype.coe_eta, dite_eq_right_iff]
+    intro h
+    exact absurd h x.property
+  -- Use this equivalence to rewrite hXcomplY (our expression for the
+  -- cardinality of Aᶜ → Y) in terms of the goal, ie the cardinality of the
+  -- extensions F : X → Y
+  have := Fintype.card_congr e
+  rw [← this] at hXcomplY
+  exact hXcomplY
 end ex4
 
 end Ch1_9
